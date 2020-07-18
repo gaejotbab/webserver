cdfc315e49caa5371b91cfd92d776155e34006a5
개좆밥 <gaejotbab@gmail.com>
2020-07-18 16:23:20 +0900

    디버깅 관련 코드 정리하고 줄 입력 함수 도입

--- server.c
+++ server.c
@@ -9,73 +9,142 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdarg.h>
+
+void log_debug(const char *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+}
 
 static const int backlog = 32;
 
-static const size_t initial_buf_capacity = 2048;
+static const size_t initial_str_buf_capacity = 64;
+static const size_t recv_buf_capacity = 2048;
 
 static bool server_stopped = false;
 
-struct socket_handler_data {
-    int accepted_socket_fd;
+struct SocketHandlerData {
+    int fd;
 };
 
-static void *accepted_socket_handler(void *arg)
-{
-    struct socket_handler_data data = *((struct socket_handler_data *)arg);
-    free(arg);
+struct RecvBuffer {
+    int fd;
 
-    printf("[%d] accepted_socket_handler\n", data.accepted_socket_fd);
+    char *buf;
+    int pos;
+    int len;
+    int cap;
+};
 
-    int buf_capacity = initial_buf_capacity;
-    char *buf = malloc(buf_capacity);
-    int buf_length = 0;
-
-    int line_feed_index = -1;
+char *recv_str_until(struct RecvBuffer *recv_buffer, char c) {
+    int str_buf_capacity = initial_str_buf_capacity;
+    char *str_buf = malloc(str_buf_capacity);
+    int str_buf_len = 0;
 
     while (1) {
-        printf("[%d] before recv\n", data.accepted_socket_fd);
+        if (recv_buffer->pos == recv_buffer->len) {
+            recv_buffer->pos = 0;
+            recv_buffer->len = recv(recv_buffer->fd, recv_buffer->buf, recv_buffer->cap, 0);
 
-        int received_length = recv(data.accepted_socket_fd,
-                buf + buf_length, buf_capacity - buf_length, 0);
-
-        if (received_length == -1) {
-            perror("recv");
-            exit(100);
+            if (recv_buffer->len == -1) {
+                perror("recv");
+                exit(100);
+            }
         }
 
-        printf("[%d] received_length=%d\n", data.accepted_socket_fd, received_length);
-
-        line_feed_index = -1;
-        for (int i = buf_length; i < buf_length + received_length; ++i) {
-            if (buf[i] == '\n') {
-                line_feed_index = i;
+        int index = -1;
+        for (int i = recv_buffer->pos; i < recv_buffer->len; ++i) {
+            if (recv_buffer->buf[i] == c) {
+                index = i;
                 break;
             }
         }
 
-        if (line_feed_index != -1) {
-            break;
-        }
+        if (index == -1) {
+            while (str_buf_len + recv_buffer->len > str_buf_capacity) {
+                str_buf_capacity *= 2;
+                str_buf = realloc(str_buf, str_buf_capacity);
+            }
 
-        buf_length += received_length;
-        if (buf_length == buf_capacity) {
-            buf_capacity *= 2;
-            buf = realloc(buf, buf_capacity);
+            memcpy(str_buf + str_buf_len, recv_buffer->buf, recv_buffer->cap);
+            str_buf_len += recv_buffer->len;
+
+            recv_buffer->pos = recv_buffer->len;
+        } else {
+            while (str_buf_len + (index + 1 - recv_buffer->pos) > str_buf_capacity) {
+                str_buf_capacity *= 2;
+                str_buf = realloc(str_buf, str_buf_capacity);
+            }
+
+            memcpy(str_buf + str_buf_len, recv_buffer->buf + recv_buffer->pos,
+                    (index + 1 - recv_buffer->pos));
+            str_buf[str_buf_len + (index + 1 - recv_buffer->pos)] = '\0';
+            str_buf_len += (index + 1 - recv_buffer->pos);
+
+            recv_buffer->pos = index + 1;
+
+            break;
         }
     }
 
+    return str_buf;
+}
+
+char *recv_line(struct RecvBuffer *recv_buffer)
+{
+    return recv_str_until(recv_buffer, '\n');
+}
+
+void remove_crlf(char *str)
+{
+    int len = strlen(str);
+
+    if (str[len] == '\n') {
+        str[len] = '\0';
+    }
+
+    if (str[len - 1] == '\r') {
+        str[len - 1] = '\0';
+    }
+}
+
+static void *accepted_socket_handler(void *arg)
+{
+    struct SocketHandlerData data = *((struct SocketHandlerData *)arg);
+    free(arg);
+
+    int fd = data.fd;
+
+    log_debug("[%d] thread started: fn=accepted_socket_handler\n", fd);
+
+    struct RecvBuffer recv_buffer = {
+        .fd = fd,
+
+        .buf = malloc(recv_buf_capacity),
+        .pos = 0,
+        .len = 0,
+        .cap = recv_buf_capacity,
+    };
+
+    char *request_line = recv_line(&recv_buffer);
+    int request_line_len = strlen(request_line);
+
     // CR position
-    buf[line_feed_index - 1] = '\0';
+    request_line[request_line_len - 1] = '\0';
+
+    log_debug("[%d] first line: %s\n", fd, request_line);
 
     char *method;
     char *request_target;
     char *http_version;
 
-    char *first_space_ptr = strchr(buf, ' ');
+    char *first_space_ptr = strchr(request_line, ' ');
     *first_space_ptr = '\0';
 
-    method = strdup(buf);
+    method = strdup(request_line);
 
     char *second_space_ptr = strchr(first_space_ptr + 1, ' ');
     *second_space_ptr = '\0';
@@ -84,19 +153,21 @@ static void *accepted_socket_handler(void *arg)
 
     http_version = strdup(second_space_ptr + 1);
 
-    printf("[%d] %s\n", data.accepted_socket_fd, buf);
-    printf("[%d]\n\tMethod: %s\n\tRequest target: %s\n\tHTTP version: %s\n",
-            data.accepted_socket_fd, method, request_target, http_version);
+    log_debug("[%d] Request line\n\tMethod: %s\n\tRequest target: %s\n\tHTTP version: %s\n",
+            fd, method, request_target, http_version);
+
+    char *first_header = recv_line(&recv_buffer);
+
+    log_debug("[%d] first header: %s\n", fd, first_header);
 
     char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, world!";
 
-    int send_result = send(data.accepted_socket_fd, http_response, strlen(http_response), 0);
+    int send_result = send(fd, http_response, strlen(http_response), 0);
     if (send_result == -1) {
         perror("send");
     }
-    printf("[%d] send_result=%d\n", data.accepted_socket_fd, send_result);
 
-    close(data.accepted_socket_fd);
+    close(fd);
 
     return NULL;
 }
@@ -137,11 +208,7 @@ int main(int argc, char **argv)
     struct sockaddr_in accepted_socket_addr;
     socklen_t accepted_socket_length = sizeof(accepted_socket_addr);
 
-    int count = 0;
-
     while (!server_stopped) {
-        printf("count: %d\n", count++);
-
         int accepted_socket_fd = accept(socket_fd,
                 (struct sockaddr *)&accepted_socket_addr, &accepted_socket_length);
 
@@ -150,11 +217,15 @@ int main(int argc, char **argv)
             exit(4);
         }
 
-        printf("accepted: return=%d\n", accepted_socket_fd);
+        log_debug("accepted: fd=%d, addr=%s:%d\n",
+                accepted_socket_fd,
+                inet_ntoa(accepted_socket_addr.sin_addr),
+                accepted_socket_addr.sin_port);
 
-        struct socket_handler_data *data = malloc(sizeof(struct socket_handler_data));
-        data->accepted_socket_fd = accepted_socket_fd;
+        struct SocketHandlerData *data = malloc(sizeof(struct SocketHandlerData));
+        data->fd = accepted_socket_fd;
 
+        /*
         pthread_t thread;
 
         int pthread_result = pthread_create(&thread, NULL, accepted_socket_handler, data);
@@ -163,8 +234,8 @@ int main(int argc, char **argv)
             perror("pthread_create");
             exit(5);
         }
-
-        printf("pthread %lu\n", thread);
+        */
+        accepted_socket_handler(data);
     }
 
     return 0;
